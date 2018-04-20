dce8faacb8ffa2900b7d02559b022469d35a63df
Saba Hassan <syedhassansaba@gmail.com>
2018-04-20 17:41:08 -0500

    added search page feature code

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+import org.apache.xerces.util.SynchronizedSymbolTable;
+
+public class SearchPage {
+	
+	public void searchPage(){
+		System.out.println("searchPage");
+		
+	}
+public void loginSet() {
+	System.out.println("login Page");
+}
+}
